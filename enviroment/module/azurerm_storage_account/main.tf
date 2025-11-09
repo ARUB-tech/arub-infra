43425887f3d7e9f1e0c31a23c4c2a2af82cb1bbf
ABDURRUB <arsiddiqui4u4@gmail.com>
2025-11-10 00:19:17 +0530

    Updated main.tf with new configuration

--- enviroment/module/azurerm_storage_account/main.tf
+++ enviroment/module/azurerm_storage_account/main.tf
@@ -8,19 +8,10 @@ resource "azurerm_storage_account" "storage_accounts" {
   access_tier              = each.value.access_tier
   is_hns_enabled           = each.value.is_hns_enabled
 
-  tags = lookup(each.value, "tags", {})
+  tags = each.value.tags
 
-  dynamic "network_rules" {
-    for_each = lookup(each.value, "network_rules",{}) 
-    # for_each =each.value.network_rules
-    content {
-      default_action             = network_rules.value.default_action
-      bypass                     = network_rules.value.bypass
-      # virtual_network_subnet_ids = network_rules.value.virtual_network_subnet_ids
-      # ip_rules                   = network_rules.value.ip_rules
-    }
-    
-  }
+ 
+
 }
 
 
